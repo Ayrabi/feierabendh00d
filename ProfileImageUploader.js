3ad517ff1185dd4cab1936a99c10f016342b5e5e
Feierabendhood <rabiaydemir2000@gmail.com>
2025-06-18 20:17:04 +0200

    Create ProfileImageUploader.js

---
+++ ProfileImageUploader.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+
+export default function ProfileImageUploader({ initialImage, canEdit, onImageChange }) {
+  // ... kompletter Code aus vorheriger Antwort ...
+}
