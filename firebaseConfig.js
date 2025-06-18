38d6a347b79c44efce9734469c3783c19957f60b
Feierabendhood <rabiaydemir2000@gmail.com>
2025-06-18 19:56:29 +0200

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Ersetze die Werte durch deine Firebase-Konfigurationsdaten aus der Firebase Console
+export const firebaseConfig = {
+  apiKey: "DEINE_API_KEY",
+  authDomain: "DEINE_AUTH_DOMAIN",
+  projectId: "DEIN_PROJECT_ID",
+  storageBucket: "DEIN_STORAGE_BUCKET",
+  messagingSenderId: "DEINE_MESSAGING_SENDER_ID",
+  appId: "DEINE_APP_ID"
+};
